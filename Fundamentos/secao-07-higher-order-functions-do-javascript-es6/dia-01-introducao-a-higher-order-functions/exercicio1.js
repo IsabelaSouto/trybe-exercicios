15fbe64a466fd81191c72a5bbf02a9f49e77d37f
IsabelaSouto <belasoutomaior@gmail.com>
2023-05-05 18:05:47 -0300

    exercicio de .find completo

--- Fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-01-introducao-a-higher-order-functions/exercicio1.js
+++ Fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-01-introducao-a-higher-order-functions/exercicio1.js
@@ -1,10 +1,17 @@
 const emailListInData = [
-    'roberta@email.com',
-    'paulo@email.com',
-    'anaroberta@email.com',
-    'fabiano@email.com',
-  ];
+  'roberta@email.com',
+  'paulo@email.com',
+  'anaroberta@email.com',
+  'fabiano@email.com',
+];
   
-  emailListInData.forEach((email) => {
-    console.log(`O email ${email} está cadastrado em nosso banco de dados!`)
-  });
+emailListInData.forEach((email) => {
+  console.log(`O email ${email} está cadastrado em nosso banco de dados!`)
+});
+
+//retornar o primeiro número do array que é divisível por 3 e 5, caso ele exista
+
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+const numeroDivisivel = (numero) =>
+  numbers.find((elemento) => elemento %3 === 0 && elemento %5 === 0);
+console.log(numeroDivisivel(numbers));
