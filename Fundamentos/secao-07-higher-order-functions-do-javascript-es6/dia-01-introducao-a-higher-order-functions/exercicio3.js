ac6a5d919ca865ad94ea8dffe25602436fe8a89e
IsabelaSouto <belasoutomaior@gmail.com>
2023-06-05 19:23:52 -0300

    exercicios finalizados do conteudo assincrono

--- Fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-01-introducao-a-higher-order-functions/exercicio3.js
+++ Fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-01-introducao-a-higher-order-functions/exercicio3.js
@@ -19,12 +19,35 @@
 // console.log(verifyFirstLetter('X', listNames)); // false
 
 // verificar se passou em todas as matérias 
-const grades = {
-    portugues: 'Aprovado',
-    matematica: 'Reprovado',
-    ingles: 'Aprovado',
-  };
+// const grades = {
+//     portugues: 'Aprovado',
+//     matematica: 'Reprovado',
+//     ingles: 'Aprovado',
+//   };
   
-  const verifyGrades = Object.values(grades).every((grade) => grade === 'Aprovado'); // false
+//   const verifyGrades = Object.values(grades).every((grade) => grade === 'Aprovado'); // false
   
-  console.log(verifyGrades);
+//   console.log(verifyGrades);
+
+// conferir a lista de pessoas convidadas de uma festa de casamento 
+// e precisa verificar se o nome da pessoa está ou não na lista
+// const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
+
+// const hasName = (arr, name) => arr.some(nomeLista => nomeLista === name);
+
+// console.log(hasName(names, 'Ana'));
+// console.log(hasName(names, 'Pedro'));
+
+// criar um sistema que verifica se as pessoas da mesma turma possuem uma determinada faixa de idade
+const people = [
+    { name: 'Mateus', age: 18 },
+    { name: 'José', age: 16 },
+    { name: 'Ana', age: 23 },
+    { name: 'Cláudia', age: 20 },
+    { name: 'Bruna', age: 19 },
+  ];
+  
+  const verifyAges = () => people.every((age) => age === 18);
+  
+  console.log(verifyAges(people, 18));
+  console.log(verifyAges(people, 14));
